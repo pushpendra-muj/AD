77acb7abd8825d080a8a72a0e0208a74a53b0343
Nishit-Thakkar <77526649+Nishit-Thakkar@users.noreply.github.com>
2023-02-02 22:52:02 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ DETA_KEY = "d03z5mkt_TafGU463A3bYRKHcWz4zP8au8bTdXUuQ"
 
 deta = Deta(DETA_KEY)
 
-db=deta.Base("Skin Cancer Detection")
+db=deta.Base("Skin_Cancer_Detection")
 
 def insert_result(name,age,address,pred):
     db.put({
